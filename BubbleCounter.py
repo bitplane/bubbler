63c3fdbbb818456733a9a08004c60ac047775619
Gaz Davidson <gaz@bitplane.net>
2012-02-07 23:00:08 +0000

    Partial refactoring
    Refactoring a bit. Doesn't run yet, just wanted to save now.

--- BubbleCounter.py
+++ BubbleCounter.py
@@ -33,28 +33,102 @@ class BubbleCounter(object):
                'FLOAT64_BE': ('>d', 8)}
 
     def __init__(self, inputFile=sys.stdin, outputFile=sys.stdout, 
-                 sampleFormat='S32_LE', sampleCount=8000, maxBubbles=5,
-                 debug=False):
+                 dataFormat='S32_LE', dataFrequency=8000, sampleTime=1000, 
+                 minTimeBetweenBubbles=50, debug=False):
         """Creates a bubble counter.
-        todo: docs
+
+        inputFile: The file to read from. This should be a RAW file containing
+            data in the format specified by sampleFormat.
+
+        outputFile: The file to write the bubble counts to. The output will be
+            one line for each samplePeriod, each containing the number of 
+            bubbles detected within this period.
+
+        dataFormat: The format of the data held in the RAW file or stream. This
+            should be one of the string keys held in BubbleCounter.FORMATS
+
+        dataFrequency: The number of samples of input data per second. For 
+            example, set this to 8000 for an 8KHZ audio stream.
+
+        sampleTime: The time in ms that the data is sampled for before 
+            each line of output. A sensible value would be 30000 or 60000, as 
+            your beer/wine/mead has just about finished fermenting when it 
+            drops to about 2 bubbles per minute.
+
+        minTimeBetweenBubbles: The minimum time between two detections that
+            will be considered a new bubble (ms). Does a gurgling noise count
+            as a bunch of discrete bubbles, or is it one big continuous one? 
+            This setting lets you decide. The default of 100ms ought to filter
+            out false positives.
         """
 
-        (structFormat, sampleSize) = BubbleCounter.FORMATS[sampleFormat]
+        # 1) Initialize IO buffers.
+        # We need to maintain a list of which files we've actually opened
+        # so that we can close them in the destructor. 
+        self.openedFiles  = []
 
-        reader = lambda : inputFile.read(sampleSize)
+        if type(inputFile) == string:
+            self.inputFile = open(inputFile, 'rb')
+            self.openedFiles.append(self.inputFile)
+        else:
+            self.inputFile = inputFile
 
-        # Initialize some variables 
-        (counter, sampleSum, sampleMean, sampleMax,
-         lastBubble, bubbleCount) = (0 for i in range(6))
+        if type(outputFile) == string:
+            self.outputFile = open(outputFile, 'wb')
+            self.openedFiles.append(self.outputFile)
+        else:
+            self.outputFile = outputFile
 
-        for sample in iter(reader, ''):
-            # abort if the sample data is the wrong length
-            if len(sample) != sampleSize:
-                break
+        # 2) Initialize input data parser
+        # Convert arecord's data format name into struct.pack format, 
+        # then create a reader that can read the data.
+        (self.dataFormat, self.dataSize) = BubbleCounter.FORMATS[sampleFormat]
+        self.dataFrequency = dataFrequency 
+        self.dataFormat    = dataFormat
+        self.dataReader    = lambda : self.inputFile.read(self.dataSize)
 
-            counter   = counter + 1
-            absSample = abs(unpack(structFormat, sample)[0])
-            sampleSum = sampleSum + absSample
+        # 3) Initialize stuff to do with the bubble counter.
+        # In future we should stick a high pass filter in this section
+
+        self.sampleTime            = sampleTime
+        self.minTimeBetweenBubbles = minTimeBetweenBubbles
+
+        # 4) Misc
+        self.debug = debug
+
+    def __del__(self):
+        """Destructor.
+        Just closes any files that we opened earlier.
+        """
+        for f in self.openedFiles:
+            if not f.closed:
+                f.close()
+
+    def start(self):
+        """Start the bubble counting process.
+        This will continue until interrupted or the end of the input
+        file or stream is reached.
+        """
+
+        # Initialize loop variables 
+        # counter, sampleSum, sampleMean, sampleMax,
+        # lastBubble, bubbleCount) = (0 for i in range(6))
+
+        sampleMean = sampleCount = 0
+
+        # iterate through the data until we reach the end
+        for rawData in iter(self.dataReader, ''):
+            try:
+                # This unpack may fail at the end of a broken stream
+                sampleValue    = unpack(structFormat, sample)[0]
+                # Use the absolute value because the average of all points of 
+                # a wave are at 0.0, which isn't very useful.
+                sampleAbs      = abs(sampleValue)
+                sampleVariance = (sampleAbs-sampleMean)**2
+                sampleSum      = sampleSum + abs(sampleValue)
+                sampleCount    = sampleCount + 1
+
+                sampleSum = sampleSum + sampleAbs
             sampleMax = sampleMax if sampleMax > absSample else absSample
 
             if counter > lastBubble + (sampleCount / maxBubbles):
@@ -115,6 +189,7 @@ def main():
     (options, args) = parser.parse_args()
 
     bubbles = BubbleCounter(**vars(options))
+    bubbles.count()
 
 if __name__ == '__main__':
     main()
--- bubbler.sh
+++ bubbler.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Pipes the default mic port into the bubble counter script, then timestamps it
-arecord --format=S32_LE --file-type=raw | ./BubbleCounter.py | ./Timestamper.py
+arecord --format=S32_LE --file-type=raw | ./BubbleCounter.py
