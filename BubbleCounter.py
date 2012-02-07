c18915f0ef9729418525ab6168115778d9b31bac
Gaz Davidson <gaz@bitplane.net>
2012-02-07 01:08:35 +0000

    Stubbed out the script files
    bubbler.sh will use alsa-utils to pipe mic data into the bubble counter,
    the bubble counter will output the bubble count for the given sample period
    to stdout. This output will be piped into some other script which will
    timestamp these numbers as they arrive.

---
+++ BubbleCounter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+"""BubbleCounter.py by Gaz Davidson 2012
+
+This script takes WAV data from stdin and counts the number of bubbles in
+a given sample period.
+"""
+import optparse
+
+def main():
+    """Main entry point into the app"""
+    # todo: add command line options, create parser
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ bubbler.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# todo: use arecord to dump the mic output to stdout,
+# then forward this to the python bubble counter.
